f4804090e1d959c6183fb3486c74a7062b4ec9f3
Grayson Kent <grayson@linux.com>
2017-04-17 07:29:22 -0400

    Cleaned up if loop and options
    Full review: https://codereview.stackexchange.com/questions/160959/try-every-option-for-a-command-and-make-anagrams-out-of-the-successful-ones

--- anagram.sh
+++ anagram.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
-#
-# Find anagrams of default options for a list of commands
-# Commentary: http://bit.ly/2ogpOJA
 
 # Pull in a list of common Linux commands
 commandList=( $( 
   curl 'http://www.thegeekstuff.com/2010/11/50-linux-commands/' 2> /dev/null \
     | grep -o '<h3>\([0-9].*[a-z][a-z][a-z].*\)</h3>' \
-    | awk '{ print $(NF-2) }' \
-    | sed -nr '/rm|wget|less|shutdown|<h3>/!p'
-) ); 
+    | awk '{ print $2 }' \
+    | grep -vE 'rm|wget|less|shutdown|<h3>'
+) );
 
 # Pipe successful "$command -$option" pairs to 'an' to generate anagrams 
-for command in ${commandList[@]} ; do 
-	(for option in {a..z} ; do
-		timeout -k 5 5 "$command" -"$option" > /dev/null 2>&1;
-			if (($? == 0)); then
-				printf "$option" | tr -d '\n'
-			else
-        			:
-			fi
-	done) | xargs an -w -d saneWordlist -m 3 2> /dev/null \
-              | sed 's/^/\ '$command' -/' >> commandOptions.log
+for command in $commandList ; do     
+        (for option in {a..z} ; do
+                if timeout -k 5 5 "$command" -$option &> /dev/null; then
+                                printf $option
+                fi      
+        done) | xargs an -w -d saneWordlist -m 3 2> /dev/null \
+              | sed "s/^/ '$command' -/" >> commandOptions.log
 done
