9b65356acf20ebe34faef7dff8278e832bcaf151
Grayson Kent <grayson@linux.com>
2017-06-16 08:04:04 -0400

    Stop abusing that poor guys website just because I am lazy

--- anagram.sh
+++ anagram.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 # Pull in a list of common Linux commands
-commandList=( $( 
-  curl 'http://www.thegeekstuff.com/2010/11/50-linux-commands/' 2> /dev/null \
-    | grep -o '<h3>\([0-9].*[a-z][a-z][a-z].*\)</h3>' \
-    | awk '{ print $2 }' \
-    | grep -vE 'rm|wget|less|shutdown|<h3>'
-) );
+commandList=(tar grep find ssh sed awk vim diff sort export xargs ls pwd cd gzip bzip2 unzip ftp crontab service ps free top df du cp mv cat mount chmod chown mkdir ifconfig uname whereis whatis locate man tail less su apt yum rpm ping date finger wget);
 
 # Pipe successful "$command -$option" pairs to 'an' to generate anagrams 
-for command in $commandList ; do     
+for command in ${commandList[@]} ; do
         (for option in {a..z} ; do
                 if timeout -k 5 5 "$command" -$option &> /dev/null; then
                                 printf $option
-                fi      
+                fi
         done) | xargs an -w -d saneWordlist -m 3 2> /dev/null \
               | sed "s/^/ '$command' -/" >> commandOptions.log
 done
