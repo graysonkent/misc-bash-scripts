a3fbbdf79de9089795726c5ca2792d72a9354530
Grayson Kent <grayson@linux.com>
2017-07-24 15:44:46 -0400

    Add explanatory block

--- anagram.sh
+++ anagram.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Script to see which commands can words out of their option flags
+# https://grayson.sh/blog/find-which-linux-commands-can-make-words-with-their-options
+
 # Pull in a list of common Linux commands
 commandList=(tar grep find ssh sed awk vim diff sort export xargs ls pwd cd gzip bzip2 unzip ftp crontab service ps free top df du cp mv cat mount chmod chown mkdir ifconfig uname whereis whatis locate man tail less su apt yum rpm ping date finger wget);
 
